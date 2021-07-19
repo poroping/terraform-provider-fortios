645981c0e4a28e149ce61f346cacf6cf61a98b7e
poroping <43107292+poroping@users.noreply.github.com>
2021-07-19 22:30:45 +0000

    update vendor

--- vendor/github.com/poroping/forti-sdk-go/fortios/sdkcore/sdkfos.go
+++ vendor/github.com/poroping/forti-sdk-go/fortios/sdkcore/sdkfos.go
@@ -25834,3 +25834,57 @@ func (c *FortiSDKClient) ReadFirewallAccessProxyVirtualHost(mkey string, vdompar
 	mapTmp, err = read(c, HTTPMethod, path, false, vdomparam, urlparams, batchid)
 	return
 }
+
+// CreateVpnsslAuthenticationRule API operation for FortiOS creates a new SSL VPN Authentication Rule.
+// Returns the index value of the SSL VPN Authentication Rule and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the vpn.ssl settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateVpnsslAuthenticationRule(data *map[string]interface{}, vdomparam string, urlparams map[string][]string, batchid int) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/vpn.ssl/settings/authentication-rule"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, data, output, vdomparam, urlparams, batchid)
+	return
+}
+
+// UpdateVpnsslAuthenticationRule API operation for FortiOS updates the specified SSL VPN Authentication Rule.
+// Returns the index value of the SSL VPN Authentication Rule and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the vpn.ssl settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateVpnsslAuthenticationRule(data *map[string]interface{}, mkey string, vdomparam string, urlparams map[string][]string, batchid int) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/vpn.ssl/settings/authentication-rule"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, data, output, vdomparam, urlparams, batchid)
+	return
+}
+
+// DeleteVpnsslAuthenticationRule API operation for FortiOS deletes the specified SSL VPN Authentication Rule.
+// Returns error for service API and SDK errors.
+// See the vpn.ssl settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteVpnsslAuthenticationRule(mkey string, vdomparam string, batchid int) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/vpn.ssl/settings/authentication-rule"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path, vdomparam, batchid)
+	return
+}
+
+// ReadVpnsslAuthenticationRule API operation for FortiOS gets the SSL VPN Authentication Rule
+// with the specified index value.
+// Returns the requested SSL VPN Authentication Rule value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the vpn.ssl settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadVpnsslAuthenticationRule(mkey string, vdomparam string, urlparams map[string][]string, batchid int) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/vpn.ssl/settings/authentication-rule"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false, vdomparam, urlparams, batchid)
+	return
+}
