5d7579e1725bcd4c5107fbd4da3e15f9cac9d8a4
poroping <43107292+poroping@users.noreply.github.com>
2022-01-03 10:29:26 +0000

    add area test/workaround

---
+++ v2/examples/resources/router.ospf_area/resource.tf
@@ -0,0 +1,24 @@
+# resource "fortios_routerospf_area" "example" {
+#   fosid = "0.0.0.0"
+# }
+
+### This resource may not work due to an API issue
+### https://github.com/poroping/terraform-provider-fortios/issues/7
+### WORKAROUND:
+
+resource "fortios_router_ospf" "example" {
+  area {
+    id = "0.0.0.0"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      "redistribute",
+      "network",
+      "neighbor",
+      "ospf_interface",
+      "summary_address",
+      "distribute_list"
+    ]
+  }
+}
